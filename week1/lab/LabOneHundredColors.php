e3abab989bbc230fe0b3b08bb79ead965ea9f12a
gregclavell-wm <gregclavell@gmail.com>
2015-09-20 21:48:36 -0400

    FixedOneHundredColors

--- week1/lab/LabOneHundredColors.php
+++ week1/lab/LabOneHundredColors.php
@@ -12,19 +12,19 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <table><tbody><tr>
-        <?php for($i=0; $i<10; $i++): ?>
-          
+        <table><tbody>
+        <?php for($i=0; $i<10; $i++):?>
             <tr>
-            <?php for($i=0; $i<10; $i++): ?>
-
+            <?php for($j=0; $j<10; $j++): ?>
+			<?php /*$randcol randomly generates a hex value to represent value. The following line creates the table cells using the color as it's background */?>
                 <?php $randcol = str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT) . str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT) . str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT); ?>
-                <td style="background-color:#'<?php.$randcol.?>';">'<?php.$randcol.?>'<br /><span style="color:#ffffff;">'<?php.$randcol.?>'</span></td>
-            <?php endfor?>
+                <td style="background-color:#<?php echo $randcol ?>;">'<?php echo $randcol ?>'<br /><span style="color:#ffffff;">'<?php echo $randcol ?>'</span></td>
+            <?php endfor;?>
             </tr>
-        
-        <?php endfor; ?>
-        </tr></tbody></table>
+            
+         <?php endfor;?>   
+      
+        </tbody></table>
     </body>
 </html>
 
